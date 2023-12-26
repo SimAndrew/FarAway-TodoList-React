1696ae1d769d11a019837f9a7a0153cf9607c8a5
AndSim <siminiucandrew@gmail.com>
2023-12-26 13:39:16 +0200

    feat:(App) build structure of project, create components of app: (Logo), (Form), (PackingList), (Stats), add css styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import './App.css';
 
-function App() {
-	return <h1>Far Away React</h1>;
+export default function App() {
+	return (
+		<div className="app">
+			<Logo />
+			<Form />
+			<PackingList />
+			<Stats />
+		</div>
+	);
 }
 
-export default App;
+function Logo() {
+	return <h1>🌴 Far Away React 💼</h1>;
+}
+
+function Form() {
+	return (
+		<div className="add-form">
+			<h3>What do you need for your trip?</h3>
+		</div>
+	);
+}
+
+function PackingList() {
+	return <div className="list">LIST</div>;
+}
+
+function Stats() {
+	return (
+		<footer className="stats">
+			<em>💼 You have X items on your list, and you already packed X (X %)</em>
+		</footer>
+	);
+}
