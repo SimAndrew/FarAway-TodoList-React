70e12a5359c4b64c33295b11ea4e2225bf800b16
AndSim <siminiucandrew@gmail.com>
2023-12-27 14:34:22 +0200

    feat:(App) render (PackingList) list, (Item) is packed = true in (initialItems) it will be struck through in the list.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import './App.css';
+import React from 'react';
+
+const initialItems = [
+	{ id: 1, description: 'Passports', quantity: 2, packed: false },
+	{ id: 2, description: 'Socks', quantity: 12, packed: false },
+	{ id: 3, description: 'Charger', quantity: 1, packed: true },
+];
 
 export default function App() {
 	return (
@@ -24,7 +31,26 @@ function Form() {
 }
 
 function PackingList() {
-	return <div className="list">LIST</div>;
+	return (
+		<div className="list">
+			<ul className="list">
+				{initialItems.map((item) => (
+					<Item item={item} key={item.id} />
+				))}
+			</ul>
+		</div>
+	);
+}
+
+function Item({ item }) {
+	return (
+		<li>
+			<span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+				{item.quantity} {item.description}{' '}
+			</span>
+			<button>❌</button>
+		</li>
+	);
 }
 
 function Stats() {
